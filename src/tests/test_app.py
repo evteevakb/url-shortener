0e856cc5c47f64a8eb6ac9178c9ac9108a3f470e
Ksenia Evteeva <k.evteeva@skoltech.ru>
2023-11-02 11:53:52 +0300

    Replace explicit endpoints with app.url_path_for

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -3,16 +3,17 @@ from httpx import AsyncClient
 from fastapi import status
 import pytest
 
+from main import app
+
 
 pytestmark = pytest.mark.asyncio
 
-TEST_INITIAL_URL = ("https://stackoverflow.com/questions/30256386/" +
-                    "how-to-copy-multiple-files-in-one-layer-using-a-dockerfile")
+TEST_INITIAL_URL = ("https://practicum.yandex.ru/")
 
 
 async def test_ping_database(client: AsyncClient) -> None:
-    """Test GET /database/ping endpoint"""
-    response = await client.get('api/v1/database/ping')
+    """Test GET endpoint for a database ping"""
+    response = await client.get(app.url_path_for('ping_database'))
     assert response.status_code == status.HTTP_200_OK
     response = response.json()
     assert 'ping_time' in response
@@ -21,18 +22,20 @@ async def test_ping_database(client: AsyncClient) -> None:
 
 
 async def test_create_short_url(client: AsyncClient) -> None:
-    """Test POST /url_shortener endpoint"""
-    response = await client.post('api/v1/url_shortener/', json={'initial_url': TEST_INITIAL_URL})
+    """Test POST endpoint for creation of a shortened URL"""
+    response = await client.post(app.url_path_for('create_short_url'),
+                                 json={'initial_url': TEST_INITIAL_URL})
     assert response.status_code == status.HTTP_201_CREATED
     response = response.json()
     assert set(['id', 'initial_url', 'short_url']).issubset(response.keys())
 
 
 async def test_get_initial_url(client: AsyncClient) -> None:
-    """Test GET /url_shortener/{url_id} endpoint"""
-    response = await client.post('api/v1/url_shortener/', json={'initial_url': TEST_INITIAL_URL})
-    url_id = response.json()['id']
-    response = await client.get(f'api/v1/url_shortener/{url_id}')
+    """Test GET endpoint for obtaining initial URL"""
+    response = await client.post(app.url_path_for('create_short_url'),
+                                 json={'initial_url': TEST_INITIAL_URL})
+    response = await client.get(app.url_path_for('get_initial_url',
+                                                 url_id=response.json()['id']))
     assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
     response = response.json()
     assert 'initial_url' in response
@@ -40,27 +43,31 @@ async def test_get_initial_url(client: AsyncClient) -> None:
 
 
 async def test_get_usage_status(client: AsyncClient) -> None:
-    """Test GET /url_shortener/{url_id}/status endpoint"""
-    response = await client.post('api/v1/url_shortener/', json={'initial_url': TEST_INITIAL_URL})
+    """Test GET endpoint for checking URL usage status"""
+    response = await client.post(app.url_path_for('create_short_url'),
+                                 json={'initial_url': TEST_INITIAL_URL})
     url_id = response.json()['id']
-    response = await client.get(f'api/v1/url_shortener/{url_id}/status')
+    response = await client.get(app.url_path_for('get_usage_status', url_id=url_id))
     assert response.status_code == status.HTTP_200_OK
     assert isinstance(response.json(), int)
-    response = await client.get(f'api/v1/url_shortener/{url_id}/status?True')
+    response = await client.get(app.url_path_for('get_usage_status', url_id=url_id),
+                                params='full-info=True')
     assert response.status_code == status.HTTP_200_OK
 
 
 async def test_delete_url(client: AsyncClient) -> None:
-    """Test DELETE /url_shortener/{url_id} endpoint"""
-    response = await client.post('api/v1/url_shortener/', json={'initial_url': TEST_INITIAL_URL})
+    """Test DELETE endpoint for marking URL as deleted"""
+    response = await client.post(app.url_path_for('create_short_url'),
+                                 json={'initial_url': TEST_INITIAL_URL})
     url_id = response.json()['id']
-    response = await client.delete(f'api/v1/url_shortener/{url_id}')
+    response = await client.delete(app.url_path_for('delete_url', url_id=url_id))
     assert response.status_code == status.HTTP_200_OK
-    response = await client.post('api/v1/url_shortener/', json={'initial_url': TEST_INITIAL_URL})
+    response = await client.post(app.url_path_for('create_short_url'),
+                                 json={'initial_url': TEST_INITIAL_URL})
     assert response.status_code == status.HTTP_410_GONE
-    response = await client.get(f'api/v1/url_shortener/{url_id}')
+    response = await client.get(app.url_path_for('get_initial_url', url_id=url_id))
     assert response.status_code == status.HTTP_410_GONE
-    response = await client.get(f'api/v1/url_shortener/{url_id}/status')
+    response = await client.get(app.url_path_for('get_usage_status', url_id=url_id))
     assert response.status_code == status.HTTP_200_OK
-    response = await client.delete(f'api/v1/url_shortener/{url_id}')
+    response = await client.delete(app.url_path_for('delete_url', url_id=url_id))
     assert response.status_code == status.HTTP_410_GONE
